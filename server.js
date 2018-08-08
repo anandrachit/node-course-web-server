490b60f245e1e9c3a7f52a9c5dbcb3dd56e4ac50
Rachit Anand <rachitanand@ip-63-107.wvu.edu>
2018-08-08 17:00:51 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+// Middleware declarations
+app.use(express.static(__dirname+'/public'));
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url} \n`;
+    fs.appendFile('server.log',log, (err) => {
+        if(err){
+            console.log('Unable to write to server.log');
+        }
+    })
+    console.log(`${log}`);
+    next();
+})
+
+// app.use((req, res, next) => {
+//     res.render('maintainance.hbs',{
+//         pageTitle: 'Maintainance in Progress'
+//     })
+// })
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>');
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Something went wrong'
+    });
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
