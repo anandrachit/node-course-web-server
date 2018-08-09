3ea544aa302c8628082a5d7669e999d843977fd2
Rachit Anand <rachitanand@Rachits-MacBook-Pro.local>
2018-08-08 22:44:04 -0400

    Adding new Project Page

--- server.js
+++ server.js
@@ -55,6 +55,12 @@ app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Something went wrong'
     });
+});
+
+app.get('/projects',(req,res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    })
 })
 
 app.listen(port, () => {
